d8f729d22872193189c94052190b370d427eb4f9
Unknown <luka.furlan9@gmail.com>
2018-12-02 14:17:34 +0100

    Day 2 part 2

--- src/main/java/com/lukafurlan/adventofcode2018/Day2.java
+++ src/main/java/com/lukafurlan/adventofcode2018/Day2.java
@@ -1,6 +1,7 @@
 package com.lukafurlan.adventofcode2018;
 
 import com.lukafurlan.adventofcode2018.helpers.FileReadHelper;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ public class Day2 {
     public static void main(String[] args) {
         entries = FileReadHelper.writeLinesToList("Day2Input.txt");
         System.out.println(getChecksum());
+        System.out.println(getCommonLetters());
     }
 
     private static int getChecksum() {
@@ -37,6 +39,40 @@ public class Day2 {
             }
         }
         return 0;
+    }
+
+    private static String getCommonLetters() {
+
+        for(int i = 0; i < entries.size(); i++) {
+            for(int j = 0; j < entries.size(); j++) {
+                if(i == j) {
+                    continue;
+                }
+
+                int numberOfDifferences = 0;
+                char differentChar = 0;
+
+                char[] firstBoxID = entries.get(i).toCharArray();
+                char[] secondBoxID = entries.get(j).toCharArray();
+
+                for(int k = 0; k < firstBoxID.length; k++) {
+
+                    if(firstBoxID[k] != secondBoxID[k]) {
+                        numberOfDifferences++;
+                        differentChar = secondBoxID[k];
+                    }
+
+                }
+
+                if(numberOfDifferences == 1) {
+                    return String.valueOf(ArrayUtils.removeElement(secondBoxID, differentChar));
+                }
+
+            }
+        }
+
+        return "Could find matching boxes.";
+
     }
 
 }
